d96326e76d89deb3f593ab3ef696ae586991b964
Max Shapira <max.shapira@icloud.com>
2025-06-02 11:38:23 +0300

    create react router application and install UI libraries

---
+++ frontend/app/routes/home.tsx
@@ -0,0 +1,14 @@
+import { Welcome } from "../common/pages/welcome";
+import type { Route } from "./+types/home";
+
+// biome-ignore lint/correctness/noEmptyPattern: <explanation>
+export function meta({}: Route.MetaArgs) {
+	return [
+		{ title: "New React Router App" },
+		{ name: "description", content: "Welcome to React Router!" },
+	];
+}
+
+export default function Home() {
+	return <Welcome />;
+}
