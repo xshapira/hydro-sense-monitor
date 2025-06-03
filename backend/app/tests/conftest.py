ccaf5f6dd8df1a8a25482a09cdd75f640f5d92f4
Max Shapira <max.shapira@icloud.com>
2025-06-03 04:05:20 +0300

    set up pytest for backend testing

---
+++ backend/app/tests/conftest.py
@@ -0,0 +1,12 @@
+import pytest
+from httpx import ASGITransport, AsyncClient
+
+from app.main import app
+
+
+@pytest.fixture
+async def client():
+    async with AsyncClient(
+        transport=ASGITransport(app=app), base_url="http://test"
+    ) as client:
+        yield client
